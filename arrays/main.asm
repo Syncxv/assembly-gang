135405f4eeaa9803a27a81a6b5758563daa9ec70
Syncxv <aruldevarackal1@gmail.com>
2023-06-25 13:47:50 +1000

    interesting

--- arrays/main.asm
+++ arrays/main.asm
@@ -1,10 +1,9 @@
 global _main
 extern _printf
+extern _GetProcessHeap@0
 
 section .data
-    my_array resd 10 ; 40 bytes of memory
-    bru db "%d", 10, 0
-
+    dbg db "thing: %d", 10, 0
 
 section .text
 
@@ -12,42 +11,12 @@ _main:
     push ebp
     mov ebp, esp ; the prologue :sus
 
-    mov ecx, 0
-    mov ebx, 0
-_init_loop:
-    cmp ecx, 10
-    je _exit_init_loop
+    call _GetProcessHeap@0
 
-    mov eax, ecx
-    mov edx, 4
-    mul edx ; eax *= ebx (byte_index = index * 4)
+    push eax
+    push dbg
+    call _printf
 
-    mov [my_array+eax], ecx ; my_array[byte_index] = index
-    add ecx, 1
-
-    jmp _init_loop
-_exit_init_loop:
-
-    mov eax, 0
-_print_loop:
-    cmp eax, 40
-    je _exit
-    
-    push eax ; to save eax value. printf does some stuff with it and changes it :raised
-
-    push dword [my_array+eax]
-    push bru
-    call _printf ; printf("%d\n", my_array[byte_index]) (or eax)
-    add esp, 8 ; restore stack (free the 8 bytes from the stack)
-
-    pop eax ; restore eax to original eax
-
-    add eax, 4 ; eax += 4
-
-    jmp _print_loop
-
-
-_exit:
     mov eax, 0 ; make the return value for main be 0
 
     mov esp, ebp ; the epilogue
