a695c4b4814c4698e8d0bea9531b463b54413b56
Syncxv <aruldevarackal1@gmail.com>
2024-03-05 03:12:18 +1100

    game loop

--- breakout/main.asm
+++ breakout/main.asm
@@ -1,9 +1,17 @@
 global _main
+extern _Sleep@4
 %include "../consoleutils/main.asm"
 
 section .data
-    player db 15 dup(219), 0
+    SLEEP_TIME equ 5
+
+    PLAYER_WIDTH equ 15
+    PLAYER_WIDTH_HALF equ PLAYER_WIDTH / 2
+    player db PLAYER_WIDTH dup(219), 0
     playerPos dd 0 ; COORD {x, y}
+
+    ball db 'o', 0
+    ballPos dd 0 ; COORD {x, y}
 
 section .text
 
@@ -11,16 +19,48 @@ _main:
     call InitConsole
     call ClearConsole
 
-    call InitPlayerPos
+    call GameMain
 
     xor eax, eax
     ret
 
+
+GameMain:
+    call InitPlayerPos
+
+    .game_loop:
+        call ClearConsole
+
+        push dword [playerPos]
+        push player
+        call PrintStrAtPos
+
+        push dword [ballPos]
+        push ball
+        call PrintStrAtPos
+
+        call GameSleep
+        jmp .game_loop
+
+    .exit:
+        ret
+
+GameSleep:
+    push dword SLEEP_TIME
+    call _Sleep@4
+
+    ret
+
 InitPlayerPos:
+    push ebp
+    mov ebp, esp
+
     mov ax, [windowWidth]
     mov bx, 2
     xor edx, edx
     div bx
+
+    sub ax, PLAYER_WIDTH_HALF
 
     push ax
     push word [windowHeight]
@@ -28,9 +68,9 @@ InitPlayerPos:
 
     mov [playerPos], eax
 
-    push eax
-    push player
-    call PrintStrAtPos
+    mov esp, ebp
+    pop ebp
+    ret
 
 SetCoord: ; [ebp+8] = y, [ebp+10] = x
     push ebp
