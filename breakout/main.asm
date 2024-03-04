b104735bb6c15d711cce8d99249eff571a50aded
Syncxv <aruldevarackal1@gmail.com>
2024-03-05 02:46:53 +1100

    intruiging

--- breakout/main.asm
+++ breakout/main.asm
@@ -7,8 +7,29 @@ _main:
     call InitConsole
     call ClearConsole
 
-    mov eax, 69420
-    call PrintDec
+    push word 50 ; x
+    push word 2 ; y
+    call SetCord
+
+    push eax
+    push 69
+    call PrintDecPos
 
     xor eax, eax
+    ret
+
+
+SetCord: ; [ebp+8] = y, [ebp+10] = x
+    push ebp
+    mov ebp, esp
+
+    mov ax, word [ebp+10]
+
+    shl eax, 16
+    mov ax, [ebp+8]
+
+    ror eax, 16
+    
+    mov esp, ebp
+    pop ebp
     ret
